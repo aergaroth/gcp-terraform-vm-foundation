bbd80ef307ae431d12a3fc7107957745bc720498
aergaroth <grochowski.sa@gmail.com>
2025-12-31 08:15:35 +0100

    chore: initialize aws foundation branch

--- variables.tf
+++ variables.tf
@@ -1,45 +1,12 @@
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
 }
 
-variable "region" {
-  description = "GCP region"
-  type        = string
+variable "instances" {
+  description = "EC2 instances definition"
+  type = map(object({
+    instance_type = string
+    az            = string
+  }))
 }
-
-variable "network_name" {
-  description = "Name of the VPC network"
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
-}
-
-variable "subnet_cidr" {
-  description = "CIDR range for the subnet"
-  type        = string
-}
-
-variable "allowed_ssh_cidrs" {
-  type = list(string)
-}
-
-variable "service_account_name" {
-  type = string
-}
-
-variable "instance_name" {
-  type = string
-}
-
-variable "machine_type" {
-  type = string
-}
-
-variable "zone" {
-  type = string
-}
-
