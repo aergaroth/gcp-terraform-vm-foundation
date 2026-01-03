1e647eee08f3fe9afaf764bc14cc1ee9c87aefe0
aergaroth <grochowski.sa@gmail.com>
2026-01-03 21:04:29 +0100

    feat(iam): replace managed SSM policy with custom least-privilege permissions

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -11,12 +11,18 @@ resource "aws_iam_role" "ec2_ssm" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ssm" {
-  role       = aws_iam_role.ec2_ssm.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+resource "aws_iam_policy" "ssm_instance_minimal" {
+  name        = "ssm-instance-minimal"
+  description = "Minimal permissions for EC2 SSM Session Manager"
+  policy      = file("${path.module}/policies/ssm-instance-minimal.json")
 }
 
 resource "aws_iam_instance_profile" "this" {
   name = "ec2-ssm-profile"
   role = aws_iam_role.ec2_ssm.name
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_minimal" {
+  role       = aws_iam_role.ec2_ssm.name
+  policy_arn = aws_iam_policy.ssm_instance_minimal.arn
 }
