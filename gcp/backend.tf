87125a1b68d610420861c359d1cc901e4f36b6ba
aergaroth <grochowski.sa@gmail.com>
2026-01-04 15:27:21 +0100

    chore(multicloud): remove broken import (will reimport from v2

--- gcp/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "tfstate-gcp-vm-foundation-dev"
-    prefix = "env/dev"
-  }
-}
--- gcp/modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "service_account_name" {
-  description = "Service account name (without domain)"
-  type        = string
-}
-
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-}
